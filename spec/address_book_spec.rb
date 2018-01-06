1d8bde59d8b5c106bfa7482830caa24b610e33a2
Elisa Mendoza <ElisaMendoza@Elisas-MacBook-Air.local>
2018-01-06 15:52:14 -0800

    Assignment 6 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -84,6 +84,36 @@ RSpec.describe AddressBook do
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
 
+    it "imports the correct number of entries from entries_2.csv" do
+
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+
+      check_entry(entry_one, "Al", "408-821-2824", "almendoza25@gmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Elisa", "925-285-3161", "elisa.mendoza@gmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Nathan", "408-270-2389", "nate24@gmail.com")
+    end
+
+
   end
 
   describe "#remove_entry" do
